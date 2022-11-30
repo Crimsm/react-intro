e979922245ed8f7961aacd4f7f61edfd64420e67
Crimsm <c.danniau27@gmail.com>
2022-11-30 10:32:43 +0100

    debug code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 import TodoList from './components/TodoList';
 
 function App() {
-  return 
-  (
-    <TodoList/>
+  return(
+    <div>
+      <TodoList />
+    </div>
   );
 }
 
